34ca7485f546161bb33ecc84a87fe591c3bf1c1d
$.m.lnk <smjvlab@gmail.com>
2018-01-09 19:56:02 +0200

    Less2 DZ1 - the largest number of four

--- src/ua/jvjab/smlnk/Runer.java
+++ src/ua/jvjab/smlnk/Runer.java
@@ -15,46 +15,47 @@ public class Runer {
 
     /**
      * @param args the command line arguments: a,b,c,d - numbers are set by the
-     * user max - the largest of the four numbers
+     * user max1 - the largest of the four numbers
      */
-    int a;
-    int b;
-    int c;
-    int d;
-    int max;
-
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите первое число: ");
-        int a = sc.nextInt();
-        System.out.println("Введите второе число: ");
-        int b = sc.nextInt();
-        System.out.println("Введите третье число: ");
-        int c = sc.nextInt();
-        System.out.println("Введите четвертое число: ");
-        int d = sc.nextInt();
+        int a;
+        int b;
+        int c;
+        int d;
 
-        int max = a;
-        if (max > b & max > c & max > d) {
-            System.out.println("Найбольшее число: " + max);
-        } else {
-            if ((b > a & b > c & b > d)) {
-                System.out.println("Найбольшее число: " + b);
-            } else {
-                if ((d > a & d > b & d > c)) {
-                    System.out.println("Найбольшее число: " + d);
-                } else {
-                    if ((c > a & c > b & c > d)) {
-                         System.out.println("Найбольшее число: " + c);
-                    } else {
-                        System.out.println("Найбольшее число не определено!");
-                    }
-                    
-                }
-            }
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Введите первое число: ");
+            a = sc.nextInt();
+            System.out.println("Введите второе число: ");
+            b = sc.nextInt();
+            System.out.println("Введите третье число: ");
+            c = sc.nextInt();
+            System.out.println("Введите четвертое число: ");
+            d = sc.nextInt();
+            sc.close();
         }
-        sc.close();
+
+        System.out.println("Наибольшее число = " + max1(a, b, c, d));
     }
 
+    public static int max1(int a, int b, int c, int d) {
+
+        if (a > b & a > c & a > d) {
+            return a;
+        } else {
+            if (b > a & b > c & b > d) {
+                return b;
+            } else {
+                if (c > a & c > b & c > d) {
+                    return c;
+                } else {
+                    return d;
+                }
+
+            }
+
+        }
+
+    }
 }
