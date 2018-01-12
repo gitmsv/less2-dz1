98f94e12655417fcbf73fbba356f148261282d59
$.m.lnk <smjvlab@gmail.com>
2018-01-12 15:10:13 +0200

    This class displays the largest of the four numbers

--- src/ua/jvjab/smlnk/Runer.java
+++ src/ua/jvjab/smlnk/Runer.java
@@ -18,42 +18,35 @@ public class Runer {
      * user max1 - the largest of the four numbers
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
         int a;
         int b;
         int c;
         int d;
 
         Scanner sc = new Scanner(System.in);
-            System.out.println("Введите первое число: ");
-            a = sc.nextInt();
-            System.out.println("Введите второе число: ");
-            b = sc.nextInt();
-            System.out.println("Введите третье число: ");
-            c = sc.nextInt();
-            System.out.println("Введите четвертое число: ");
-            d = sc.nextInt();
-            sc.close();
-            System.out.println("Наибольшее число = " + max1(a, b, c, d));
-    }
+        System.out.println("Введите первое число: ");
+        a = sc.nextInt();
+        System.out.println("Введите второе число: ");
+        b = sc.nextInt();
+        System.out.println("Введите третье число: ");
+        c = sc.nextInt();
+        System.out.println("Введите четвертое число: ");
+        d = sc.nextInt();
+        sc.close();
 
-    public static int max1(int a, int b, int c, int d) {
+        int max;
+        max = a;
 
-        if (a > b & a > c & a > d) {
-            return a;
-        } else {
-            if (b > a & b > c & b > d) {
-                return b;
-            } else {
-                if (c > a & c > b & c > d) {
-                    return c;
-                } else {
-                    return d;
-                }
-
-            }
-
+        if (max < b) {
+            max = b;
         }
-
+        if (max < c) {
+            max = c;
+        }
+        if (max < d) {
+            max = d;
+        }
+        System.out.println("Наибольшим числом есть: " + max);
     }
 }
